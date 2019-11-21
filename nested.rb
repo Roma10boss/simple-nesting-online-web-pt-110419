c77ea9271ebcd10e1044d17f64d84f8161f4f62e
Romario Gustave <romagustave10@gmail.com>
2019-11-21 00:48:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -113,7 +113,7 @@ def changing_alan
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
-          :known_for => "Unix",
+          :known_for => " "GUI"Unix",
           :languages => ["C"]
         }
      }
